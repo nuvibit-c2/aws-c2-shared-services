de0c435619d44855eebc8dcd27994a5b07a30ed1
stefano-franco <stefano.franco@nuvibit.com>
2024-10-24 11:45:30 +0200

    create alias record

--- ntc_r53_spacelift_nuvibit_dev.tf
+++ ntc_r53_spacelift_nuvibit_dev.tf
@@ -19,13 +19,16 @@ module "ntc_r53_spacelift_nuvibit_dev" {
   # https://docs.aws.amazon.com/Route53/latest/DeveloperGuide/resource-record-sets-choosing-alias-non-alias.html
   dns_records = [
     {
-      # placeholder APEX record will be replaced with ALB alias record
+      # apex record for spacelift self-hosted loadbalancer - check 'spacelift_self_hosted.tf'
       name = ""
       type = "A"
       ttl  = 300
-      values = [
-        "53.53.53.53"
-      ]
+      alias = {
+        enable_alias           = true
+        target_dns_name        = data.aws_lb.spacelift.dns_name
+        target_hosted_zone_id  = data.aws_lb.spacelift.zone_id
+        evaluate_target_health = true
+      }
     }
   ]
 
